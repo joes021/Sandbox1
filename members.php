fcfdf536fc3d38c216afbae94c119c3fbe4f01fb
joes021 <joes021@yahoo.com>
2016-11-30 16:28:25 +0100

    Add files via upload

--- members.php
+++ members.php
@@ -1,1 +1,17 @@
-idbsdubcs
+<?php
+class Members{
+  Public $id;
+  Public $name;
+  Public $age;
+
+
+function __construct ($id, $name, $age){
+
+  $this->id=$id;
+  $this->name=$name;
+  $this->age=$age;
+}
+
+
+
+}
--- players.php
+++ players.php
@@ -1,1 +1,13 @@
-afsdfs
+<?php
+
+require_once("members.php");
+
+class Players extends Members{
+  function __construct($id, $name, $age, $teamId){
+    $this->teamId=$teamId;
+    parent::__construct($id, $name, $age);
+  }
+
+  public $teamId;
+
+}
